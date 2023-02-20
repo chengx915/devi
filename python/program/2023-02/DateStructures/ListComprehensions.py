8d69d58e2ff70f86e3fb02d49680fa735a98f3a8
devil2077xin@126.com <Xx970915>
2023-02-20 17:53:29 +0800

    update

--- python/program/2023-02/DateStructures/ListComprehensions.py
+++ python/program/2023-02/DateStructures/ListComprehensions.py
@@ -1,2 +1,11 @@
 # 列表推导式创建列表的方式更简洁。
 # 常见的用法为，对序列或可迭代对象中的每个元素应用某种操作，用生成的结果创建新的列表；或用满足特定条件的元素创建子序列。
+squares = []
+for x in range(10):
+    squares.append(x ** 2)
+
+squares
+
+squares = list(map(lambda x: x ** 3, range(10)))
+
+squares = [x ** 4 for x in range(10)]
